3361449e63b1639bcb7a94932b25b6cfee9364ca
eduardouno1 <eduardojoseespinoza2006@gmail.com>
2026-03-15 20:51:36 -0400

    Se configura Sreamlit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 
 # Configuración de la Página de Streamlit
 st.set_page_config(
-    page_title="Dashboard de Streamers de Twitch",
+    page_title="Dashboard de Consumo de Alcohol a nivel mundial",
     layout="wide",
     initial_sidebar_state="expanded"
 )
@@ -14,4 +14,5 @@ st.set_page_config(
 PALETTE_TWITCH = ["#6441a5", "#7f61bc", "#a690dd", "#b9baff", "#F0F0F0", "#000000"]
 PALETTE_COLOURS = ["#6441a5", "#26C6DA", "#FF1493", "#FAFF00", "#5DFF00"]
 PRIMARY_COLOR = PALETTE_COLOURS[0]
+
 
